0ff70bf050bdde59f862ae65855b1112abdfdea0
xxzarkx <rodrigofranco.1301@gmail.com>
2025-08-05 18:22:11 -0500

    modificando jwt

--- src/main/java/com/helpdesk/helpdesk/security/JwtFilter.java
+++ src/main/java/com/helpdesk/helpdesk/security/JwtFilter.java
@@ -2,15 +2,15 @@ package com.helpdesk.helpdesk.security;
 
 import com.helpdesk.helpdesk.model.Usuario;
 import com.helpdesk.helpdesk.repository.UsuarioRepository;
-import com.helpdesk.helpdesk.security.JwtUtil;
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -33,19 +33,23 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String jwt = authHeader.substring(7);
-            if (jwtUtil.validarToken(jwt)) {
-                String correo = jwtUtil.extraerCorreo(jwt);
-                Usuario usuario = usuarioRepository.findByCorreo(correo).orElse(null);
+            String correo = jwtUtil.extraerCorreo(jwt);
 
-                if (usuario != null) {
-                    UsuarioDetails usuarioDetails = new UsuarioDetails(usuario);
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(usuarioDetails, null, usuarioDetails.getAuthorities());
-
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+            if (correo != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                if (jwtUtil.validarToken(jwt)) {
+                    Usuario usuario = usuarioRepository.findByCorreo(correo).orElse(null);
+                    if (usuario != null) {
+                        UsuarioDetails usuarioDetails = new UsuarioDetails(usuario);
+                        UsernamePasswordAuthenticationToken authToken =
+                                new UsernamePasswordAuthenticationToken(
+                                        usuarioDetails,
+                                        null,
+                                        usuarioDetails.getAuthorities()
+                                );
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
                 }
-
             }
         }
 
